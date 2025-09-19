f73db5038d8e88a34d7108f0a75b529c09dcdddf
123047802-cyber <123047802@upq.edu.mx>
2025-09-18 21:17:49 -0600

    Create ejercicioC.js

---
+++ Practica1/ejercicioC.js
@@ -0,0 +1,5 @@
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+console.log(saludoPersonalizado("Isay", 37));
+console.log(saludoPersonalizado("Ana", 25));
+console.log(saludoPersonalizado("Luis", 40));
